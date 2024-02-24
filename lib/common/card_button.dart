c9570ce2e6fed1c799e37574d25c46c15268ebf5
julianb393 <julian.b@live.ca>
2024-02-24 13:30:10 -0500

    Added basic settings page

--- lib/common/card_button.dart
+++ lib/common/card_button.dart
@@ -4,16 +4,18 @@ class CardButton extends StatelessWidget {
   final String title;
   final IconData? icon;
   final void Function() onTap;
-  const CardButton({
-    super.key,
-    required this.title,
-    this.icon,
-    required this.onTap,
-  });
+  final Color? color;
+  const CardButton(
+      {super.key,
+      required this.title,
+      this.icon,
+      required this.onTap,
+      this.color});
 
   @override
   Widget build(BuildContext context) {
     return Card(
+      color: color,
       child: InkWell(
         onTap: onTap,
         child: ListTile(
