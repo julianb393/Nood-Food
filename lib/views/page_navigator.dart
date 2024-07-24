47a1e093782496531ed0f61e539d87faf3d9443e
Julian Barker <julian.b@live.ca>
2024-07-23 21:47:26 -0400

    fix rebuild loop from setState in stream builder of page navigator

--- lib/views/page_navigator.dart
+++ lib/views/page_navigator.dart
@@ -22,15 +22,19 @@ class _PageNavigatorState extends State<PageNavigator> {
   late final DBService _dbService;
   double dailyCaloriesAllowed = 3200.0;
   DateTime _selectedDay = DateTime.now();
-  Widget _accountIcon = const Icon(Icons.person_outlined);
-  Widget _accountIconActive = const Icon(Icons.person);
+  late Widget _accountIcon;
+  late Widget _accountIconActive;
+  late String? accIconUrl;
 
   int _selectedPageIndex = 0;
   final List<String> _pageTitles = ['Nood Food', 'Meals', 'Account'];
 
   // Will allow the home to control the date, which can get shared with others.
   void _updateSelectedDate(DateTime newDate) {
-    setState(() => _selectedDay = newDate);
+    setState(() {
+      _selectedDay = newDate;
+      print('rebuilding');
+    });
   }
 
   DateTime _getSelectedDate() {
@@ -40,29 +44,29 @@ class _PageNavigatorState extends State<PageNavigator> {
   void _setAccountIcon(String? photoURL) {
     if (photoURL == null) return;
     // Will be triggered after build is complete.
-    WidgetsBinding.instance.addPostFrameCallback((_) => setState(() {
-          Widget profilePic = CachedNetworkImage(
-            imageUrl: photoURL,
-            placeholder: (context, val) => const Loader(),
-            errorWidget: (context, url, error) => const Icon(Icons.error),
-            imageBuilder: (context, imageProvider) {
-              return CircleAvatar(
-                radius: 18,
-                backgroundImage: imageProvider,
-              );
-            },
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      Widget profilePic = CachedNetworkImage(
+        imageUrl: photoURL,
+        placeholder: (context, val) => const Loader(),
+        errorWidget: (context, url, error) => const Icon(Icons.error),
+        imageBuilder: (context, imageProvider) {
+          return CircleAvatar(
+            radius: 18,
+            backgroundImage: imageProvider,
           );
-          _accountIcon = CircleAvatar(
-            backgroundColor: Colors.transparent,
-            radius: 20,
-            child: profilePic,
-          );
-          _accountIconActive = CircleAvatar(
-            backgroundColor: Colors.greenAccent,
-            radius: 20,
-            child: profilePic,
-          );
-        }));
+        },
+      );
+      _accountIcon = CircleAvatar(
+        backgroundColor: Colors.transparent,
+        radius: 20,
+        child: profilePic,
+      );
+      _accountIconActive = CircleAvatar(
+        backgroundColor: Colors.greenAccent,
+        radius: 20,
+        child: profilePic,
+      );
+    });
   }
 
   @override
@@ -70,6 +74,8 @@ class _PageNavigatorState extends State<PageNavigator> {
     super.initState();
     _authService = AuthService();
     _dbService = DBService(uid: _authService.userUid);
+    _accountIcon = const Icon(Icons.person_outlined);
+    _accountIconActive = const Icon(Icons.person);
   }
 
   @override
@@ -77,54 +83,71 @@ class _PageNavigatorState extends State<PageNavigator> {
     return StreamProvider<List<Food>>.value(
       value: _dbService.getFoodsFromDate(_selectedDay),
       initialData: const [],
-      child: Scaffold(
-        appBar: AppBar(title: Text(_pageTitles[_selectedPageIndex])),
-        bottomNavigationBar: BottomNavigationBar(
-          showSelectedLabels: false,
-          showUnselectedLabels: false,
-          iconSize: 25,
-          items: <BottomNavigationBarItem>[
-            const BottomNavigationBarItem(
-              icon: Icon(Icons.home_outlined),
-              activeIcon: Icon(Icons.home),
-              label: 'Home',
-            ),
-            const BottomNavigationBarItem(
-              icon: Icon(Icons.fastfood_outlined),
-              activeIcon: Icon(Icons.fastfood),
-              label: 'Meals',
-            ),
-            BottomNavigationBarItem(
-              icon: _accountIcon,
-              activeIcon: _accountIconActive,
-              label: 'Account',
-            ),
-          ],
-          backgroundColor: Colors.black54,
-          elevation: 0,
-          currentIndex: _selectedPageIndex,
-          selectedItemColor: const Color.fromARGB(255, 170, 231, 220),
-          onTap: (index) => setState(() => _selectedPageIndex = index),
-        ),
-        body: StreamBuilder(
+      child: StreamBuilder(
           stream: _dbService.userAccountInfo,
           builder: (context, AsyncSnapshot<NFUser?> snapshot) {
             if (!snapshot.hasData) {
               return const Loader();
             }
-            _setAccountIcon(snapshot.data!.photoURL);
-            return [
-              Home(
-                user: snapshot.data!,
-                updateDate: _updateSelectedDate,
-                getSelectedDate: _getSelectedDate,
-              ),
-              Meals(day: _selectedDay),
-              Account(user: snapshot.data!),
-            ][_selectedPageIndex];
-          },
-        ),
-      ),
+            Widget profilePic = CachedNetworkImage(
+              imageUrl: snapshot.data!.photoURL!,
+              placeholder: (context, val) => const Loader(),
+              errorWidget: (context, url, error) =>
+                  const Icon(Icons.person_outlined),
+              imageBuilder: (context, imageProvider) {
+                return CircleAvatar(
+                  radius: 18,
+                  backgroundImage: imageProvider,
+                );
+              },
+            );
+            return Scaffold(
+                appBar: AppBar(title: Text(_pageTitles[_selectedPageIndex])),
+                bottomNavigationBar: BottomNavigationBar(
+                  showSelectedLabels: false,
+                  showUnselectedLabels: false,
+                  iconSize: 25,
+                  items: <BottomNavigationBarItem>[
+                    const BottomNavigationBarItem(
+                      icon: Icon(Icons.home_outlined),
+                      activeIcon: Icon(Icons.home),
+                      label: 'Home',
+                    ),
+                    const BottomNavigationBarItem(
+                      icon: Icon(Icons.fastfood_outlined),
+                      activeIcon: Icon(Icons.fastfood),
+                      label: 'Meals',
+                    ),
+                    BottomNavigationBarItem(
+                      icon: CircleAvatar(
+                        backgroundColor: Colors.transparent,
+                        radius: 20,
+                        child: profilePic,
+                      ),
+                      activeIcon: CircleAvatar(
+                        backgroundColor: Colors.greenAccent,
+                        radius: 20,
+                        child: profilePic,
+                      ),
+                      label: 'Account',
+                    ),
+                  ],
+                  backgroundColor: Colors.black54,
+                  elevation: 0,
+                  currentIndex: _selectedPageIndex,
+                  selectedItemColor: const Color.fromARGB(255, 170, 231, 220),
+                  onTap: (index) => setState(() => _selectedPageIndex = index),
+                ),
+                body: [
+                  Home(
+                    user: snapshot.data!,
+                    updateDate: _updateSelectedDate,
+                    getSelectedDate: _getSelectedDate,
+                  ),
+                  Meals(day: _selectedDay),
+                  Account(user: snapshot.data!),
+                ][_selectedPageIndex]);
+          }),
     );
   }
 }
